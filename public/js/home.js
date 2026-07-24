a6942d8d54ce7ce39543e3dd761ce1e453eaa431
Soham <sohamghosh2925@gmail.com>
2026-07-24 20:50:47 +0530

    Day 7

--- public/js/home.js
+++ public/js/home.js
@@ -1,71 +1,47 @@
 let blogs = [];
 
 async function loadBlogs() {
-
     const container = document.getElementById("blogs");
-
     container.innerHTML = "<h2>Loading...</h2>";
-
     try {
-
         const response = await fetch("/blogs");
-
         if (!response.ok) {
             throw new Error("Failed to load blogs");
         }
 
         blogs = await response.json();
-
         container.innerHTML = "";
-
         if (blogs.length === 0) {
-
             container.innerHTML = `
                 <div class="empty">
                     No Blogs Found
                 </div>
             `;
-
             return;
         }
 
         blogs.forEach(blog => {
-
             const card = document.createElement("div");
-
             card.className = "card";
-
             card.innerHTML = `
-
                 <img src="${blog.image}" alt="${blog.title}">
-
                 <div class="card-content">
-
                     <span class="category">
                         ${blog.category}
                     </span>
-
                     <h3>
                         ${blog.title}
                     </h3>
-
                     <p>
                         ${blog.content}
                     </p>
-
                     <p class="author">
                         ${blog.author}
                     </p>
-
                     <div class="btn-group">
-                        <button class="btn-read">
-                            <span>Read</span>
-                        </button>
-
                         <button class="btn-edit">
                             Edit
                         </button>
-
                         <!-- From Uiverse.io by vinodjangid07 --> 
                         <button class="btn-delete">
                         <svg
@@ -86,7 +62,6 @@ async function loadBlogs() {
                             </clipPath>
                             </defs>
                         </svg>
-
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -106,118 +81,73 @@ async function loadBlogs() {
                             </defs>
                         </svg>
                         </button>
-
-
                     </div>
-
                 </div>
-
             `;
 
             card.querySelector(".btn-edit").addEventListener("click", () => {
-
                 editBlog(blog.id);
-
             });
 
             card.querySelector(".btn-delete").addEventListener("click", () => {
-
                 deleteBlog(blog.id);
-
             });
-
             container.appendChild(card);
-
         });
-
     }
 
     catch (err) {
-
         console.error(err);
-
         container.innerHTML = `
             <div class="empty">
                 Error Loading Blogs
             </div>
         `;
-
     }
-
 }
 
 loadBlogs();
 
 async function deleteBlog(id) {
-
     const ok = confirm("Are you sure you want to delete this blog?");
-
     if (!ok) return;
-
     const response = await fetch(`/blogs/${id}`, {
-
         method: "DELETE"
-
     });
 
     const data = await response.json();
-
     alert(data.message);
-
     loadBlogs();
-
 }
 
 async function editBlog(id) {
-
     const blog = blogs.find(b => b.id === id);
-
     if (!blog) return;
-
     const title = prompt("Enter Blog Title", blog.title);
-
     if (title === null) return;
-
     const author = prompt("Enter Author", blog.author);
-
     if (author === null) return;
-
     const category = prompt("Enter Category", blog.category);
-
     if (category === null) return;
-
     const description = prompt("Enter Description", blog.description);
-
     if (description === null) return;
-
     const content = prompt("Enter Content", blog.content);
-
     if (content === null) return;
-
     const response = await fetch(`/blogs/${id}`, {
-
         method: "PUT",
-
         headers: {
             "Content-Type": "application/json"
         },
-
         body: JSON.stringify({
-
             title,
             author,
             category,
             description,
             content
-
         })
-
     });
 
     const data = await response.json();
-
     alert(data.message);
-
     loadBlogs();
-
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const multer = require("multer");
 const cors = require("cors");
 const fs = require("fs");
+const Table = require("cli-table3");
 
 const app = express();
 const PORT = 3000;
@@ -20,54 +21,43 @@ if (!fs.existsSync(uploadPath)) {
 const FILE_PATH = path.join(__dirname, "blogs.json");
 
 function readBlogs() {
-
     if (!fs.existsSync(FILE_PATH)) {
         fs.writeFileSync(FILE_PATH, "[]");
     }
-
     return JSON.parse(fs.readFileSync(FILE_PATH, "utf8"));
 }
 
 function writeBlogs(blogs) {
-
     fs.writeFileSync(
         FILE_PATH,
         JSON.stringify(blogs, null, 4)
     );
-
 }
 
 // ================= MULTER =================
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadPath);
     },
 
     filename: (req, file, cb) => {
-        cb(null, "img_" + file.originalname);
+        cb(null,Date.now().toString().slice(-5) + "_" + file.originalname);
     }
 });
 
 const upload = multer({ storage });
 
 // ================= PAGES =================
-
 app.get("/", (req, res) => {
-
     res.sendFile(path.join(__dirname, "public", "home.html"));
-
 });
 
 app.get("/add_blog", (req, res) => {
-
     res.sendFile(path.join(__dirname, "public", "add_blog.html"));
-
 });
 
 
 // ================= API =================
-
 app.get("/status", (req, res) => {
     res.json({
         status: `Server is Running Successfully at http://localhost:${PORT}`,
@@ -77,7 +67,6 @@ app.get("/status", (req, res) => {
 });
 
 // ================= ADD BLOG =================
-
 app.post("/add_blog", upload.single("image"), (req, res) => {
     console.log(req.body)
     const {
@@ -91,46 +80,53 @@ app.post("/add_blog", upload.single("image"), (req, res) => {
     const blogs = readBlogs();
 
     const blog = {
-        Id: Date.now(),
-        Title: title,
-        Author:author,
-        Category:category,
-        Description:description,
-        Content:content,
-        Image: req.file ? "/uploads/" + req.file.filename : ""
+        id: Date.now(),
+        title: title,
+        author:author,
+        category:category,
+        description:description,
+        content:content,
+        image: req.file ? "/uploads/" + req.file.filename : ""
     };
 
-    blogs.push(blog);
-
+    blogs.unshift(blog);
     writeBlogs(blogs);
+
+    const table = new Table({
+        head: ["ID", "Title", "Author", "Category", "Description", "Image"]
+    });
+
+    console.log("\n\t\t\t\t\t\t\tNEW BLOG PUBLISHED");
+
+    blogs.forEach((item) => {
+        table.push([
+            item.id,
+            item.title.length > 20
+                ? item.title.substring(0, 17) + "..."
+                : item.title,
+            item.author,
+            item.category,
+            item.description.length > 25
+                ? item.description.substring(0, 22) + "..."
+                : item.description,
+            item.image
+        ]);
+    });
+
+    console.log(table.toString());
 
     res.json({
         success: true,
         message: "Blog Added Successfully",
         data: blogs
     });
-
-    console.log("\n\t\tNew Blog Added");
-    console.table({
-        Id: blog.Id,
-        Title: blog.Title,
-        Author: blog.Author,
-        Category: blog.Category,
-        Description: blog.Description,
-        Content: blog.Content,
-        Image: blog.Image
-    });
 });
 
+app.get("/blogs", (req, res) => {
+    res.json(readBlogs());
+});
 
 // ================= SERVER =================
-
 app.listen(PORT, () => {
-
-    console.log(
-
-        `Server Running : http://localhost:${PORT}`
-
-    );
-
+    console.log(`Server Running : http://localhost:${PORT}`);
 });
